1caa2ee557cd8fb8c96fce9bec8a547b2ad90e62
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-09-05 02:31:01 -0500

    fix: delete many credentials to tests

--- test/utils/prisma.utils.ts
+++ test/utils/prisma.utils.ts
@@ -2,6 +2,7 @@ import { PrismaService } from '../../src/prisma/prisma.service';
 
 export class PrismaUtils {
   static async cleanDb(prisma: PrismaService) {
+    await prisma.credential.deleteMany();
     await prisma.user.deleteMany();
   }
 }
